19e349b6e60f1f50dfe1135d1ae57a572a29ee8c
mfarzamalam <mfarzamalam@gmail.com>
2021-09-11 03:34:55 +0500

    setup our django app with functional_test

---
+++ superlist/functional_tests.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+browser.get('http://localhost:8000')
+
+assert 'install' in browser.title
