86673f26dc9eea3a5416a6e7e54fde2b13bc992a
mfarzamalam <mfarzamalam@gmail.com>
2021-09-15 03:27:14 +0500

    using unittest in functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,16 +1,27 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
-browser = webdriver.Chrome(ChromeDriverManager().install())
-
-    # alex has heard about the new cool to-do app
-    # he decided to visits its homepage by clicking on the url
-browser.get('http://localhost:8000')
+import unittest
 
 
-    # he notice the website title has a word To-Do 
-assert 'To-Do' in browser.title, "Title is: "+"'"+ browser.title +"'"
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+
+    def tearDown(self):
+        # when his work is complete he close the browser
+        self.browser.quit()
+        
+    def test_can_start_list_and_retrieve_it_later(self):
+        # alex has heard about the new cool to-do app
+        # he decided to visits its homepage by clicking on the url
+        self.browser.get('http://localhost:8000')
 
 
-    # when his work is complete he close the browser
-browser.quit()
+        # he notice the website title has a word To-Do 
+        self.assertIn ('To-Do', self.browser.title)
+        self.fail("Finish the test!")
+
+
+if __name__ == '__main__':
+    unittest.main()
