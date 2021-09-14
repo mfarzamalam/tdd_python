afba6114d1c04fb4312fae7b18dc640cef3dea4e
mfarzamalam <mfarzamalam@gmail.com>
2021-09-15 03:16:30 +0500

    added a user story in functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -2,8 +2,15 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
+
+    # alex has heard about the new cool to-do app
+    # he decided to visits its homepage by clicking on the url
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title, "Title is: "+"'"+ browser.title +"'"
 
-browser.close()
+    # he notice the website title has a word To-Do 
+assert 'To-Do' in browser.title, "Title is: "+"'"+ browser.title +"'"
+
+
+    # when his work is complete he close the browser
+browser.quit()
