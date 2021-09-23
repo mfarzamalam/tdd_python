c6439a49e270fad3d453773bd9ee8dd10064781e
mfarzamalam <mfarzamalam@gmail.com>
2021-09-23 16:59:13 +0500

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,10 +19,30 @@ class NewVisitorTest(unittest.TestCase):
         # he decided to visits its homepage by clicking on the url
         self.browser.get('http://localhost:8000')
 
-
         # he notice the website title has a word To-Do 
         self.assertIn ('To-Do', self.browser.title)
+
+        # he notice the website header has a word To-Do 
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # He invited to fill the To-Do list
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputBox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # He types 'Play the Piano' in the text-box and hit enter
+        inputBox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_table_list')
+        rows  = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Play the Piano') for row in rows)
+
+        # There's still a text-box asking him to enter another item.
+        # He enter another item 'Come back home with dhaniya'
         self.fail("Finish the test!")
+
+        # The page updates again when she press the enter and show both item in his list
 
 
 if __name__ == '__main__':
