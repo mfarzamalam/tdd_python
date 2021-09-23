d60b1b047df7dc6af67a9aa9b04aef49aa85c8b0
mfarzamalam <mfarzamalam@gmail.com>
2021-09-23 16:31:15 +0500

    Added unit test of home_page and write the view

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,8 @@
-from django.test import TestCase
+from datetime import timedelta
 from django.urls import resolve
+from django.test import TestCase
+from django.http import HttpRequest, request
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -14,3 +17,12 @@ class HomePageTest(TestCase):
     def test_root_url_show_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+
+    def test_home_page_return_correct_html(self):
+        request  = HttpRequest()
+        response = home_page(request)
+        html     = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
